9d01625f0e14f1e3551ae8297d4b7d24c9258b0a
tynchtykkg <tynchtyk.estebesuulu@gmail.com>
2023-11-01 16:36:53 +0600

    second commit

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -6,8 +6,21 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      backgroundColor: Colors.blueGrey,
       body: Center(
-        child: Text("Hello, World!!!"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '0',
+              style: TextStyle(
+                fontSize: 48,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 255, 255, 255),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
