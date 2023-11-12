ae527c3a9d34f8da123ba25643e32d4a9ff1a213
tynchtykkg <tynchtyk.estebesuulu@gmail.com>
2023-11-12 21:09:27 +0600

    textfield widget done

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -5,18 +5,44 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final border = OutlineInputBorder(
+      borderSide: const BorderSide(
+        width: 2,
+        style: BorderStyle.solid,
+      ),
+      borderRadius: BorderRadius.circular(
+        10,
+      ),
+    );
+    return Scaffold(
       backgroundColor: Colors.blueGrey,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               '0',
               style: TextStyle(
                 fontSize: 48,
                 fontWeight: FontWeight.bold,
                 color: Color.fromARGB(255, 255, 255, 255),
+              ),
+            ),
+            TextField(
+              style: const TextStyle(
+                color: Colors.black,
+              ),
+              decoration: InputDecoration(
+                hintText: 'Please enter the amount in USD',
+                hintStyle: const TextStyle(
+                  color: Colors.black,
+                ),
+                prefixIcon: const Icon(Icons.monetization_on_rounded),
+                prefixIconColor: Colors.black,
+                filled: true,
+                fillColor: Colors.white,
+                focusedBorder: border,
+                enabledBorder: border,
               ),
             ),
           ],
